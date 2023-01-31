5f0b9749f17b96185e665938cc474327adb453fc
Dirk <bubbels_xd@hotmail.com>
2023-01-30 21:18:55 -0600

    Added game header and how to play section

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div style={{margin: "20px", fontFamily: "'VT323', monospace", fontSize: "56px"}}>
+            BADMINTON
+        </div>
         <Shuttlecock />
+        <div style={{margin: "20px", fontFamily: "'VT323', monospace", fontSize: "24px"}}>
+            How to play: WASD to move, Use mouse position to aim crosshair, when birdie is close to you and low enough, left click to hit. Backspace to restart game.
+        </div>
       </header>
     </div>
   );
